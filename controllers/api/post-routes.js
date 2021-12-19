59dcd851684c31529a63405428389b6f5d43e9ba
trujilml <mickeytrj@gmail.com>
2021-12-19 00:31:24 -0600

    started working on the routes for the homepage and dashpage alongside for the user and comment

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+//get all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at'
+        ],
+        order: [
+            'created_at',
+            'DESC'
+        ],
+        include: [
+        {
+            model: User,
+            attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
+module.exports = router;
