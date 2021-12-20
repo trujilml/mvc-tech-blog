d027c3df136b18918de1d3b5709871c2822116a5
trujilml <mickeytrj@gmail.com>
2021-12-20 01:16:22 -0600

    continued typing down for routes - should start work on models and handlebar webpages and partials

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -36,6 +36,61 @@ router.get('/', (req, res) => {
     });
 });
 
+//Get a single post
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found!'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//create a post
+router.post('/', (req, res) => {
+    console.log("Creating post...");
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 
 
 module.exports = router;
