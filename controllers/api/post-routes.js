0c2d727f3e2291b780902124008535cb17f5b905
trujilml <mickeytrj@gmail.com>
2021-12-26 01:59:59 -0600

    completed the post-routes for the tech blog

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -116,6 +116,26 @@ router.put('/:id', withAuth, (req, res) => {
 });
 
 //Delete a post
+router.delete(':/id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+    .then((dbPostData) => {
+        if(!dbPostData) {
+            res.status(404).json({
+                message: 'No post found!'
+            });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 
 module.exports = router;
